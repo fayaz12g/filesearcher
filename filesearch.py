51ad488abcc63a00b5193d9e51dd1b2dc5977476
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-09 02:15:32 -0400

    Update filesearch.py

--- filesearch.py
+++ filesearch.py
@@ -10,7 +10,7 @@ ctk.set_appearance_mode("system")  # Set the theme of the app
 ctk.set_default_color_theme("dark-blue")  # Set the color theme
 root = ctk.CTk()  # Create the main window
 root.title('File Searcher')
-root.geometry("530x880")
+root.geometry("530x980")
 
 script_path = os.path.abspath(__file__)
 script_directory = os.path.dirname(script_path)
@@ -21,12 +21,26 @@ root.iconbitmap(icon_path)
 def find_files_by_extension(directory, file_extension, textbox):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if search_extension_checkbox.get() and file.endswith(file_extension):
-                file_path = os.path.join(root, file)
-            elif not search_extension_checkbox.get():
-                file_path = os.path.join(root, file)
+            # Adjust the comparison based on case sensitivity
+            if case_sensitive.get():
+                match_string = search_entry.get()
+                match_file = file
+                match_extension = file_extension
             else:
+                match_string = search_entry.get().lower()
+                match_file = file.lower()
+                match_extension = file_extension.lower()
+
+            # Check if we need to search for a specific string in the file names
+            if search_entry.get() and match_string not in match_file:
                 continue
+
+            # Check if we need to search for files with a specific extension
+            if search_extension_checkbox.get() and not match_file.endswith(match_extension):
+                continue
+
+            # Construct the file path
+            file_path = os.path.join(root, file)
 
             # Determine how to display the file path based on display_name
             if display_name.get() == "Full":
@@ -44,6 +58,9 @@ def find_files_by_extension(directory, file_extension, textbox):
 
     textbox.insert('end', "\n\nEnd of File Search")
 
+    progressbar.stop()
+
+
 
 # Function to handle the search process
 def start_search_thread():
@@ -60,6 +77,8 @@ def start_search():
     directory = filedialog.askdirectory()
     if directory:  # If a directory is selected
         result_textbox.delete('1.0', 'end')  # Clear the textbox
+        progressbar.start()
+        result_textbox.insert('end', "Begining Search. You will get another message when the search \nfinishes\n\n")
         find_files_by_extension(directory, file_extension, result_textbox)
     result_textbox.configure(state='disabled') 
 
@@ -73,8 +92,14 @@ search_label = ctk.CTkLabel(root, text="Search String:", font=('Futura',15))
 search_tooltip = CTkToolTip(search_label, message="(leave empty to list all files)")
 search_label.pack()
 
-search_entry = ctk.CTkEntry(root, placeholder_text="")
+search_entry = ctk.CTkEntry(root)
 search_entry.pack(pady=5)
+
+case_sensitive = ctk.CTkSwitch(root, text = "Case Sensitive")
+case_sensitive.pack()
+
+newlabel = ctk.CTkLabel(root, text="", font=('Futura',15))
+newlabel.pack(pady=5)
 
 display_label = ctk.CTkLabel(root, text="Display Result As:", font=('Futura',15))
 display_tooltip = CTkToolTip(display_label, message=f'''Full = Full Path to the file
@@ -82,8 +107,12 @@ Truncated = Truncated path starting from the folder you choose
 Bare = File name and extension only''')
 display_label.pack()
 
-display_name = ctk.CTkComboBox(root, values=["Full", "Truncated", "Bare"])
-display_name.pack(pady=10)
+display_name = ctk.CTkSegmentedButton(root, values=["Full", "Truncated", "Bare"], width=300, dynamic_resizing=True)
+display_name.set("Truncated")
+display_name.pack(pady=5)
+
+newlabel = ctk.CTkLabel(root, text="", font=('Futura',15))
+newlabel.pack(pady=5)
 
 search_extension_checkbox = ctk.CTkCheckBox(root, text="Search by file extension:")
 extension_tooltip = CTkToolTip(search_extension_checkbox, message="(leave unchecked to search all files)")
@@ -107,8 +136,12 @@ search_button.pack(pady=10)
 
 result_textbox.pack(pady=20)
 
-display_label2 = ctk.CTkLabel(root, text="Hover Over Options for More Info", font=('Futura',10))
+progressbar = ctk.CTkProgressBar(root, orientation="horizontal", progress_color="#800080", mode='indeterminate', indeterminate_speed=2, corner_radius=10, width=300)
+progressbar.pack()
+
+display_label2 = ctk.CTkLabel(root, text="Hover Over Each Option for More Informoation", font=('Futura',10))
 display_label2.pack()
+
 
 # Run the application
 root.mainloop()
