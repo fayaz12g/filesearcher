54ef65ecc42a23d326e3802350a80f37c2ceb131
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-05-09 01:43:34 -0400

    Update filesearch.py

--- filesearch.py
+++ filesearch.py
@@ -3,13 +3,14 @@ import threading
 from customtkinter import *
 import customtkinter as ctk
 from tkinter import filedialog
+from CTkToolTip import *
 
 # Initialize the main window
 ctk.set_appearance_mode("system")  # Set the theme of the app
 ctk.set_default_color_theme("dark-blue")  # Set the color theme
 root = ctk.CTk()  # Create the main window
 root.title('File Searcher')
-root.geometry("530x830")
+root.geometry("530x880")
 
 script_path = os.path.abspath(__file__)
 script_directory = os.path.dirname(script_path)
@@ -46,6 +47,7 @@ def find_files_by_extension(directory, file_extension, textbox):
 
 # Function to handle the search process
 def start_search_thread():
+    result_textbox.configure(state='normal') 
     # Start the search in a new thread
     search_thread = threading.Thread(target=start_search)
     search_thread.start()
@@ -59,22 +61,32 @@ def start_search():
     if directory:  # If a directory is selected
         result_textbox.delete('1.0', 'end')  # Clear the textbox
         find_files_by_extension(directory, file_extension, result_textbox)
+    result_textbox.configure(state='disabled') 
 
 # UI Elements
 titlebar = ctk.CTkLabel(root, text="File Searcher", font=('Futura',50))
 titlebar.pack(pady=50)
 result_textbox = ctk.CTkTextbox(root, height=350, width=400)
+result_textbox.configure(state='disabled')  # Initially disabled
 
-search_label = ctk.CTkLabel(root, text="Search for string (leave empty to list all files)", font=('Futura',15))
+search_label = ctk.CTkLabel(root, text="Search String:", font=('Futura',15))
+search_tooltip = CTkToolTip(search_label, message="(leave empty to list all files)")
 search_label.pack()
 
+search_entry = ctk.CTkEntry(root, placeholder_text="")
+search_entry.pack(pady=5)
+
 display_label = ctk.CTkLabel(root, text="Display Result As:", font=('Futura',15))
+display_tooltip = CTkToolTip(display_label, message=f'''Full = Full Path to the file
+Truncated = Truncated path starting from the folder you choose
+Bare = File name and extension only''')
 display_label.pack()
 
 display_name = ctk.CTkComboBox(root, values=["Full", "Truncated", "Bare"])
 display_name.pack(pady=10)
 
-search_extension_checkbox = ctk.CTkCheckBox(root, text="Search by file extension (leave unchecked to search all files)")
+search_extension_checkbox = ctk.CTkCheckBox(root, text="Search by file extension:")
+extension_tooltip = CTkToolTip(search_extension_checkbox, message="(leave unchecked to search all files)")
 search_extension_checkbox.pack()
 
 file_extension_entry = ctk.CTkEntry(root, placeholder_text="txt")
@@ -94,6 +106,9 @@ search_button = ctk.CTkButton(root, text="Search Folder", command=start_search_t
 search_button.pack(pady=10)
 
 result_textbox.pack(pady=20)
+
+display_label2 = ctk.CTkLabel(root, text="Hover Over Options for More Info", font=('Futura',10))
+display_label2.pack()
 
 # Run the application
 root.mainloop()
